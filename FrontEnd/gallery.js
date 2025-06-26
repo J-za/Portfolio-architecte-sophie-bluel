f92d4876c974e9effc9df6a6458ffa3d09be0d1c
J-Za <jzarrouk@gmail.com>
2025-06-26 09:45:11 +0200

    refactor: rename all functions and constants from French to English

--- FrontEnd/gallery.js
+++ FrontEnd/gallery.js
@@ -15,38 +15,38 @@ async function loadWorks() {
 }
 
 //Affichage des travaux dans la galerie
-function afficherTravaux(travaux) {
+function displayWorks(works) {
     const gallery = document.querySelector(".gallery")
 
     gallery.innerHTML =""
-    
-    travaux.forEach(travail => {
 
-        const travauxElement = document.createElement("figure")         //Création d'une balise figure
+    works.forEach(work => {
 
-        const imageTravaux = document.createElement("img")              //Création d'une balise img et récupération des informations associées
-        imageTravaux.src = travail.imageUrl
-        imageTravaux.alt = travail.title
+        const workElement = document.createElement("figure")         //Création d'une balise figure
 
-        const titreTravaux = document.createElement("figcaption")       //Création d'une balise figcaption et récupération des informations associées 
-        titreTravaux.innerText = travail.title
+        const workImage = document.createElement("img")              //Création d'une balise img et récupération des informations associées
+        workImage.src = work.imageUrl
+        workImage.alt = work.title
 
-        travauxElement.appendChild(imageTravaux)                        //Rattachement des balises aux parents
-        travauxElement.appendChild(titreTravaux)
-        gallery.appendChild(travauxElement)
+        const workCaption = document.createElement("figcaption")       //Création d'une balise figcaption et récupération des informations associées 
+        workCaption.innerText = work.title
+
+        workElement.appendChild(workImage)                        //Rattachement des balises aux parents
+        workElement.appendChild(workCaption)
+        gallery.appendChild(workElement)
     })
 
 }
 
 //Création des boutons et gestion des filtres
 
-function gestionFiltres(travaux) {
+function handleFilters(works) {
 
     const gallery = document.querySelector(".gallery")
 
     //-----------------Récupération des 3 catégories-----------------//
 
-    const names = travaux.map(travail => travail.category.name);
+    const names = works.map(work => work.category.name);
     const setCategorie = new Set(names);
     const tableCategorie = [...setCategorie]
 
@@ -82,28 +82,28 @@ function gestionFiltres(travaux) {
 
     allButton.addEventListener("click", function () {
         gallery.innerHTML = ""
-        afficherTravaux(travaux)
+        displayWorks(works)
     })
     objectsButton.addEventListener("click", function () {
-        const travauxFiltres = travaux.filter(travail => travail.category.id === 1)
+        const worksFilters = works.filter(work => work.category.id === 1)
         gallery.innerHTML = ""
-        afficherTravaux(travauxFiltres)
+        displayWorks(worksFilters)
     })
     apartmentsButton.addEventListener("click", function () {
-        const travauxFiltres = travaux.filter(travail => travail.category.id === 2)
+        const worksFilters = works.filter(work => work.category.id === 2)
         gallery.innerHTML = ""
-        afficherTravaux(travauxFiltres)
+        displayWorks(worksFilters)
     })
     hotelButton.addEventListener("click", function () {
-        const travauxFiltres = travaux.filter(travail => travail.category.id === 3)
+        const worksFilters = works.filter(work => work.category.id === 3)
         gallery.innerHTML = ""
-        afficherTravaux(travauxFiltres)
+        displayWorks(worksFilters)
     })
 }
 
 // Listener sur le chargement du DOM et appel des fonctions
 document.addEventListener("DOMContentLoaded", async () => {
     const data = await loadWorks()
-    afficherTravaux(data)
-    gestionFiltres(data)
+    displayWorks(data)
+    handleFilters(data)
 })
