098110be52d6c16d6c9419cb61d06052a461b38c
J-za <jzarrouk@gmail.com>
2025-07-01 12:14:46 +0200

    feat: improve login with show/hide password and error message on invalid credentials

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -2,12 +2,28 @@ async function handleLoginFormSubmit() {
 
     const form = document.querySelector("form")
 
+    togglePasswordView()
+
     form.addEventListener("submit", async (event) => {
         event.preventDefault()
         const email = document.querySelector("#email").value
         const password = document.querySelector("#password").value
+        const emailContent = document.querySelector("#email")
+        const passwordContent = document.querySelector("#password")
+        const errorMessage = document.querySelector(".error-message")
 
-        await loginUser(email, password)
+        const success = await loginUser(email, password)
+
+
+        if (!success) {
+            errorMessage.textContent = "E-mail ou mot de passe incorrect."
+            emailContent.classList.add("error")
+            passwordContent.classList.add("error")
+        } else {
+            sessionStorage.setItem('token', success.token)
+            window.location.href = "index.html"
+        }
+
     })
 
 }
@@ -26,13 +42,29 @@ async function loginUser(email, password) {
             throw new Error(`HTTP error : ${response.status}`)
         }
         const data = await response.json()
-        sessionStorage.setItem('token', data.token)
-        window.location.href ="index.html"
+        return data
     } catch (error) {
-        alert("E-mail ou mot de passe incorrect")
         console.error(error.message)
     }
 
+}
+
+function togglePasswordView() {
+    const passwordInput = document.getElementById("password")
+    const togglePassword = document.getElementById("toggle-password")
+
+    togglePassword.addEventListener("click", () => {
+        if (passwordInput.type === "password") {
+            passwordInput.type = "texte"
+            togglePassword.classList.remove("fa-eye")
+            togglePassword.classList.add("fa-eye-slash")
+
+        } else {
+            passwordInput.type = "password"
+            togglePassword.classList.add("fa-eye")
+            togglePassword.classList.remove("fa-eye-slash")
+        }
+    })
 }
 
 document.addEventListener("DOMContentLoaded", () => {
